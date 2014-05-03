d4b87f71a4382422298c2d95eec050b5279f03b5
Stuart Olsen <sjo@lantea>
2014-05-03 00:37:05 -0500

    Added BIND
    - BIND binds arguments to a function based on numerical position; for
      example, (BIND #'NTH :1 0) would produce an object functionally
      equivalent to #'FIRST.

--- helper-macros.lisp
+++ helper-macros.lisp
@@ -2,6 +2,11 @@
 ;;;
 ;;;; Macros for general cleaning up of code. These don't really accomplish
 ;;;; anything particularly complex, but it's nice to have them.
+;;;
+;;;; FIXME: Consider adding support for BINDing keyword arguments, e.g.:
+;;;;
+;;;;    (MAPCAR (BIND #'POSITION :2 '(1 2 3 2 1) :FROM-END T) '(1 2 3))
+;;;;    => (4 3 2)
 
 (in-package :sj-lisp)
 
@@ -44,3 +49,50 @@ unique symbol."
               collecting (cons name new-name))))
       `(progn
          ,@(replace-names body name-alist)))))
+
+
+;;;; Macro BIND
+
+;;; Bind arguments to a function. This can be thought of as a generalization of
+;;; currying, but is mostly just to cut down on the proliferation of lambda
+;;; forms which simply bind an argument. Arguments are supplied with numeric
+;;; keywords, e.g.
+;;;
+;;;    (FUNCALL (BIND #'NTH :2 '(1 2 3 4)) 0)
+;;;    => 1
+;;;
+;;; This is implemented as a macro rather than a function because processing the
+;;; positional arguments is _not_ a trivial operation. The odds of it making a
+;;; difference in terms of usability are, I think, rather slim.
+(defmacro bind (function &rest keyed-args)
+  "Bind FUNCTION with the arguments specified by KEYED-ARGS. The arguments are,
+as the name suggests, keyed, specifically by position, starting from 1."
+  (labels ((keyword->index (keyword)
+             (let ((index (parse-integer (symbol-name keyword))))
+               (if (< index 1)
+                   (error 'type-error :expected-type '(integer 1) :datum index)
+                   index))))
+    (multiple-value-bind (bindings greatest-position)
+        (loop
+           for (position-keyword arg-form) on keyed-args by #'cddr
+           for position = (keyword->index position-keyword)
+           maximizing position into greatest-position
+           collecting (cons position arg-form) into bindings
+           finally (return (values (sort bindings #'< :key #'car)
+                                   greatest-position)))
+      (multiple-value-bind (lambda-list arg-forms)
+          (loop
+             for i from 1 to greatest-position
+             for binding = (find i bindings :key #'car)
+             for arg-form = (cdr binding)
+             for named-argument = (unless binding
+                                    (gensym (format nil "ARG~D-" i)))
+             if binding
+               collect arg-form into arg-forms
+             else
+               collect named-argument into lambda-list and
+               collect named-argument into arg-forms
+             finally (return (values lambda-list arg-forms)))
+        (with-hygienic-names (rest)
+          `(lambda (,@lambda-list &rest rest)
+             (apply ,function ,@arg-forms rest)))))))
